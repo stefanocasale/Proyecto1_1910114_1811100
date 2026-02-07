93ae190831e10cf9618335a0a007c6b8b41def31
stefanocasale <stefanocasale000@gmail.com>
2026-02-06 21:32:11 -0400

    metodos sencillos listos

--- ListaAdyacenciaGrafo.kt
+++ ListaAdyacenciaGrafo.kt
@@ -1,16 +1,22 @@
 class ListaAdyacenciaGrafo<T> : Grafo<T> {
     private val adyacencias: MutableMap<T, MutableList<T>> = mutableMapOf()
 
-    override fun contiene(v: T): Boolean { // STEFANO
-        return v in adyacencias
+    override fun contiene(v: T): Boolean { 
+        return v in adyacencias 
     }
 
-    override fun agregarVertice(v: T): Boolean { // STEFANO
-        return false
+    override fun agregarVertice(v: T): Boolean { 
+        if (v in adyacencias) { // Si el vertice existe, no se agrega y se retorna false
+            return false
+        } else {
+            // Se agrega el vértice con una lista vacía de adyacencias, ya que no tiene conexiones aún
+            adyacencias[v] = mutableListOf() 
+            return true
+        }
     }
 
-    override fun tamano(): Int { // STEFANO 
-        return 0
+    override fun tamano(): Int { 
+        return adyacencias.size // Devuelve la cantidad de vértices en el grafo
     }
 
     override fun conectar(desde: T, hasta: T): Boolean { // STEFANO
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Proyecto1_1910114_1811100
 Nombres: Stefano Casale, Enyerber Silva
 Carnets: 1910114, 1811100
+
+
+# Explicación de Métodos
+- contiene(v: T): Boolean: Le indica al usuario si un vertice ya es parte de la lista de adyacencias.
+
+- agregarVertice(v: T): Boolean: Cuestiona si existe el nodo que se quiere agregar. En caso de que ya exista, no hace nada; caso contrario, lo agrega como una lista vacía y mutable ya que hasta ahora, no tiene conexciones.
+ 
+- tamano(): Int: Devuelve la cantidad de objetos que hay en el conjunto de vértices, que justamente es el tamaño del mapa de adyacencias
+
+- conectar(desde: T, hasta: T): Boolean
+- obtenerArcosSalida(v: T): List<T>
+- obtenerArcosEntrada(v: T): List<T>s
+- eliminarVertice(v: T): Boolean
+- subgrafo(vertices: Collection<T>): Grafo<T>
+
+
