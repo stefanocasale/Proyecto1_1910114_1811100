665ed66cad7a4f452a269615ad850c5cce55bbd8
stefanocasale <stefanocasale000@gmail.com>
2026-02-07 18:44:01 -0400

    .

--- ListaAdyacenciaGrafo.kt
+++ ListaAdyacenciaGrafo.kt
@@ -1,41 +1,75 @@
 class ListaAdyacenciaGrafo<T> : Grafo<T> {
     private val adyacencias: MutableMap<T, MutableList<T>> = mutableMapOf()
 
-    override fun contiene(v: T): Boolean { 
+    override fun contiene(v: T): Boolean { // Verifica si el vértice existe en el grafo
         return v in adyacencias 
     }
 
     override fun agregarVertice(v: T): Boolean { 
-        if (v in adyacencias) { // Si el vertice existe, no se agrega y se retorna false
-            return false
-        } else {
-            // Se agrega el vértice con una lista vacía de adyacencias, ya que no tiene conexiones aún
+        if (v !in adyacencias) { // Si el vértice no existe, se agrega al grafo
             adyacencias[v] = mutableListOf() 
             return true
         }
+
+        return false 
     }
 
     override fun tamano(): Int { 
         return adyacencias.size // Devuelve la cantidad de vértices en el grafo
     }
 
-    override fun conectar(desde: T, hasta: T): Boolean { // STEFANO
-        return false
+    override fun conectar(desde: T, hasta: T): Boolean { 
+        // Verifica que ambos nodos a conectar existan en el grafo
+        if (desde !in adyacencias || hasta !in adyacencias){
+            return false 
+        }
+
+        // Verifica si la conexión ya existe 
+        if (hasta in adyacencias[desde]!!) {
+            return false
+        }
+
+        // Realiza la conexión   
+        adyacencias[desde]!!.add(hasta)
+
+        return true
     }
 
-    override fun obtenerArcosSalida(v: T): List<T> { // STEFANO
-        return listOf()
+    override fun obtenerArcosSalida(v: T): List<T> { 
+        // Verifica si el vértice existe 
+        if (v !in adyacencias) {
+            return listOf() // Si no existe, retorna una lista vacía
+        }
+
+        // Retorna la lista de vértices que salen desde v 
+        return adyacencias[v]!!
     }
 
-    override fun obtenerArcosEntrada(v: T): List<T> { // ENYER
-        return listOf()
+    override fun obtenerArcosEntrada(v: T): List<T> { 
+
+        // Verifica si el vértice existe
+        if (v !in adyacencias) {
+            return listOf() // Si no existe, retorna una lista vacía
+        }
+
+        val entrada = mutableListOf<T>() // Almacena los vértices que tienen una conexión hacia v
+
+        // Recorre todos los vertices 
+        for (vertices in adyacencias) { 
+            // Verifica la conexión 
+            if (v in vertices.value) { // Si v esta en la lista actual, se agrega el vértice a la lista de entrada
+                entrada.add(vertices.key) 
+            }
+        }
+
+        return entrada
     }
 
-    override fun eliminarVertice(v: T): Boolean { // ENYER
+    override fun eliminarVertice(v: T): Boolean { 
         return false 
     }
 
-    override fun subgrafo(vertices: Collection<T>): Grafo<T> { // ENYER
+    override fun subgrafo(vertices: Collection<T>): Grafo<T> { 
         return ListaAdyacenciaGrafo()
     }
 }
--- README.md
+++ README.md
@@ -5,15 +5,15 @@ Carnets: 1910114, 1811100
 # Explicación de Métodos
 - contiene(v: T): Boolean: Le indica al usuario si un vertice ya es parte de la lista de adyacencias.
 
-- agregarVertice(v: T): Boolean: Cuestiona si existe el nodo que se quiere agregar. En caso de que ya exista, no hace nada; caso contrario, lo agrega como una lista vacía y mutable ya que hasta ahora, no tiene conexciones.
+- agregarVertice(v: T): Boolean: Verifica si existe el nodo que se quiere agregar. En caso de que ya exista, no hace nada; caso contrario, lo agrega como una lista vacía y mutable ya que hasta ahora, no tiene conexiones.
  
-- tamano(): Int: Devuelve la cantidad de objetos que hay en el conjunto de vértices, que justamente es el tamaño del mapa de adyacencias
+- tamano(): Int: Devuelve la cantidad de objetos que hay en el conjunto de vértices, que justamente es el tamaño del mapa de adyacencias.
 
-- conectar(desde: T, hasta: T): Boolean:
+- conectar(desde: T, hasta: T): Boolean: Conecta dos vertices de un grafo. Si alguno de ellos no existe, o la conexión ya existe, retorna false, caso contraio retorna true.
 
-- obtenerArcosSalida(v: T): List<T>:
+- obtenerArcosSalida(v: T): List<T>: Retorna la lista de vértices que salen desde v. Si v no es un vértice del grafo o v no tiene arcos de salida, entonces retorna una lista vacía.
 
-- obtenerArcosEntrada(v: T): List<T>:
+- obtenerArcosEntrada(v: T): List<T>: Retorna la lista de vértices que llegan a v. Si v no existe, entonces el método regresará una lista vacía. Itera sobre todos los vértices, para cada uno accede a su lista de adyacencias, si v esta en esa lista, agrega el vértice a la lista que finalmente se retorna.
 
 - eliminarVertice(v: T): Boolean:
 
