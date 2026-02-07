4373e255de8c7247f68f23e5a63775c24c26c709
stefanocasale <stefanocasale000@gmail.com>
2026-02-06 20:29:29 -0400

    division de trabajo (cualquier cosa me avisas

--- Grafo.kt
+++ Grafo.kt
@@ -1,10 +1,27 @@
 interface Grafo<T> {
-    fun agregarVertice(v: T): Boolean
+    fun agregarVertice(v: T): Boolean    // STEFANO
+
+
+
+    
     fun eliminarVertice(v: T): Boolean
-    fun conectar(desde: T, hasta: T): Boolean
-    fun contiene(v: T): Boolean
+
+
+    fun conectar(desde: T, hasta: T): Boolean   // STEFANO
+
+
+    fun contiene(v: T): Boolean     // STEFANO 
+
+
     fun obtenerArcosSalida(v: T): List<T>
+
+
     fun obtenerArcosEntrada(v: T): List<T>
-    fun tamano(): Int
+
+
+    fun tamano(): Int   // STEFANO
+
+
     fun subgrafo(vertices: Collection<T>): Grafo<T>
+    
 }
