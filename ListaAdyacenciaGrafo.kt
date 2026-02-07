f8df2f13b5d2c66f740aa7a350f7a816bb400b08
stefanocasale <stefanocasale000@gmail.com>
2026-02-06 20:53:28 -0400

    Ahor si, la division

--- ListaAdyacenciaGrafo.kt
+++ ListaAdyacenciaGrafo.kt
@@ -1,35 +1,35 @@
 class ListaAdyacenciaGrafo<T> : Grafo<T> {
     private val adyacencias: MutableMap<T, MutableList<T>> = mutableMapOf()
 
-    override fun contiene(v: T): Boolean {
+    override fun contiene(v: T): Boolean { // STEFANO
         return false
     }
 
-    override fun agregarVertice(v: T): Boolean {
+    override fun agregarVertice(v: T): Boolean { // STEFANO
         return false
     }
 
-    override fun tamano(): Int {
+    override fun tamano(): Int { // STEFANO 
         return 0
     }
 
-    override fun conectar(desde: T, hasta: T): Boolean {
+    override fun conectar(desde: T, hasta: T): Boolean { // STEFANO
         return false
     }
 
-    override fun obtenerArcosSalida(v: T): List<T> {
+    override fun obtenerArcosSalida(v: T): List<T> { // STEFANO
         return listOf()
     }
 
-    override fun obtenerArcosEntrada(v: T): List<T> {
+    override fun obtenerArcosEntrada(v: T): List<T> { // ENYER
         return listOf()
     }
 
-    override fun eliminarVertice(v: T): Boolean {
-        return false
+    override fun eliminarVertice(v: T): Boolean { // ENYER
+        return false 
     }
 
-    override fun subgrafo(vertices: Collection<T>): Grafo<T> {
+    override fun subgrafo(vertices: Collection<T>): Grafo<T> { // ENYER
         return ListaAdyacenciaGrafo()
     }
 }
