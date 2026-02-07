58d667cedaf38b4af9f9a9f3c6929bc0f2824a6d
stefanocasale <stefanocasale000@gmail.com>
2026-02-06 20:15:33 -0400

    Añadi Grafo.kt

--- Grafo.kt
+++ Grafo.kt
@@ -1,2 +1,10 @@
-// Prueba de commit a traves de vscode y WSL
-
+interface Grafo<T> {
+    fun agregarVertice(v: T): Boolean
+    fun eliminarVertice(v: T): Boolean
+    fun conectar(desde: T, hasta: T): Boolean
+    fun contiene(v: T): Boolean
+    fun obtenerArcosSalida(v: T): List<T>
+    fun obtenerArcosEntrada(v: T): List<T>
+    fun tamano(): Int
+    fun subgrafo(vertices: Collection<T>): Grafo<T>
+}
